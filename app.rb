5812f6256e102f1a648383777b394e92521b9297
George Acosta <georgeacostar@gmail.com>
2019-12-20 12:56:05 -0800

    add words.erb

--- app.rb
+++ app.rb
@@ -4,3 +4,13 @@ require('./lib/word') #UPDATE PARENT CLASS
 require('pry')
 require('./lib/definition') #UPDATE CHILD CLASS
 also_reload('lib/**/*.rb')
+
+get('/') do
+  @your_choice_words = Word.all
+  erb(:words)
+end
+
+get('/words') do
+  @your_choice_words = Word.all
+  erb(:words)
+end
