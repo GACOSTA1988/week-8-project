e8dd98ea0f5c8cff56724afce85057916211cca9
George Acosta <georgeacostar@gmail.com>
2019-12-20 12:11:48 -0800

    add save method test and functionality

--- lib/word.rb
+++ lib/word.rb
@@ -1,16 +1,25 @@
 class Word
-    attr_accessor :your_choice_word
-    @@your_choice_words = []
+    attr_accessor :your_choice_word, :id
+    @@your_choice_words = {}
+    @@total_rows = 0
 
-    def initialize(your_choice_word)
+    def initialize(your_choice_word, id)
       @your_choice_word = your_choice_word
+      @id = id || @@total_rows += 1
     end
 
     def self.all()
-        @@your_choice_words
+        @@your_choice_words.values()
     end
   
     def self.find(your_choice_word)
     end
-  
+
+    def ==(word_to_compare)
+        self.your_choice_word() == word_to_compare.your_choice_word()
+      end
+
+    def save
+        @@your_choice_words[self.id] = Word.new(self.your_choice_word, self.id)
+    end
   end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -2,8 +2,30 @@ require 'rspec'
 require 'word'
 require 'definition'
 
+describe '#Word' do
 describe(".all") do
-    it("is empty at first") do
+    it("This will return an empty array") do
       expect(Word.all()).to(eq([]))
     end
   end
+
+  describe('#save') do
+  it("will save a word") do
+    test_word = Word.new("Plate", nil) 
+    test_word.save()
+    test_word2 = Word.new("Paper", nil) 
+    test_word2.save()
+    expect(Word.all).to(eq([test_word, test_word2]))
+  end
+end
+describe('#==') do
+it("Checks to see if your_choice_word is the same as another by attribute only") do
+  test_word = Word.new("Paper", nil)
+  test_word2 = Word.new("Paper", nil)
+  expect(test_word).to(eq(test_word2))
+end
+end
+end
+
+
+
